fac148b7902769f549b69d7ccc71077bc90bb01d
Sean Murphy <sean@iamseanmurphy.com>
2026-02-11 07:05:19 +0000

    4.8.0

--- jsm-show-user-meta/jsm-show-user-meta.php
+++ jsm-show-user-meta/jsm-show-user-meta.php
@@ -11,8 +11,8 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Show user metadata in a metabox when editing users - a great tool for debugging issues with user metadata.
  * Requires PHP: 7.4.33
- * Requires At Least: 5.9
- * Tested Up To: 6.8.1
+ * Requires At Least: 6.0
+ * Tested Up To: 6.9
  * Version: 4.8.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
